da03dfb367461be8f9b2f73744fb3f1fdc239bae
shobhit549 <sshobhit1106@gmail.com>
2024-08-03 22:59:51 +0530

    Created Website with navbar and footer on main repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/",
+  base:"/",//working with slash only but chatgpt saying should contain repo name
   plugins: [react()],
 });
